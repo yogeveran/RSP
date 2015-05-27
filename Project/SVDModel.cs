6a5dd6f7f20ad8752d3e74ea8fe9f0ff27daf4a2
eran yogev <yogeveran@gmail.com>
2015-05-27 14:52:46 +0300

    Update

--- Project/SVDModel.cs
+++ Project/SVDModel.cs
@@ -8,5 +8,33 @@ namespace Project
 {
     class SVDModel
     {
+        private double miu;
+
+        private Dictionary<string, double> Bi = null;
+        private Dictionary<string, double> Bu = null;
+
+        private Dictionary<string, Vector> Pu = null;
+        private Dictionary<string, Vector> Qi = null;
+
+
+        public double PredictRating(string sUID, string sIID)
+        {
+            if (Bi.ContainsKey(sIID) && Bu.ContainsKey(sUID))
+                return miu + Bi[sIID] + Bu[sUID] + (Pu[sUID] * Qi[sIID]);
+            else
+                return miu;
+        }
+
+        public SVDModel merge(SVDModel other)
+        {
+            if (other == null)
+                throw new ArgumentNullException();
+            if (Bi == null)
+                throw new InvalidOperationException("SVDModel must be trained before merge");
+        }
+
+        public void trainBaseModel(int cFeatures){
+
+        }
     }
 }
