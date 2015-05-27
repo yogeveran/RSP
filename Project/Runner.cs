d92e0b6afeef07b2decc4c17a3aa2949e0d65e1c
eran yogev <yogeveran@gmail.com>
2015-05-28 00:51:03 +0300

    Almost done

--- Project/Runner.cs
+++ Project/Runner.cs
@@ -25,7 +25,7 @@ namespace Project
                 DateTime regTime1 = DateTime.Now;
                 Runner regRun = new Runner();
                 regRun.Load(fileName, trainSetSize, 1.0);
-                SVDModel svd = regRun.RunAlgo();
+                SVDModel svd = regRun.RunAlgo(0, regRun.list.Count - 1);
                 TimeSpan span = DateTime.Now.Subtract(regTime1);
                 regularTrainList.Add(span);
                 #endregion
@@ -34,20 +34,20 @@ namespace Project
             List<TimeSpan> quickTrainList = new List<TimeSpan>();
             List<TimeSpan> fullTrainList = new List<TimeSpan>();
             Runner runner = null;
-            SVDModel model = null, fullyTrainedSVD = null;
+            SVDModel ourModel = null, fullyTrainedSVD = null;
             
             for (int i = 0; i < LOOP; i++) { 
                 #region take_algorithm_avg_loop
                 DateTime time = DateTime.Now;
                 runner = new Runner();
                 runner.Load(fileName, trainSetSize, SmallestDBSize);
-                model = runner.RunAlgo();
+                ourModel = runner.RunAlgo(0,runner.list.Count-1);
                 DateTime time2 = DateTime.Now;
-                fullyTrainedSVD = new SVDModel(model);
+                fullyTrainedSVD = new SVDModel(ourModel);
                 DateTime time3 = DateTime.Now; //Ignore Time of DeepCopyConstructor
                 fullyTrainedSVD.trainBaseModel(cFeatures);
                 DateTime time4 = DateTime.Now;
-                double sim = model.similiarity(fullyTrainedSVD);
+                double sim = ourModel.similiarity(fullyTrainedSVD);
     
                 TimeSpan quickTrain = time2.Subtract(time);
                 TimeSpan fullTrain = time4.Subtract(time).Subtract(time3.Subtract(time2));
@@ -58,12 +58,15 @@ namespace Project
                 #endregion
             }
             double dConfidence,ourRMSE,svdRMSE;
-            runner.Compute_RMSE_and_Confidence(model, fullyTrainedSVD, out dConfidence, out ourRMSE, out svdRMSE);
+            runner.Compute_RMSE_and_Confidence(ourModel, fullyTrainedSVD, out dConfidence, out ourRMSE, out svdRMSE);
+            Console.WriteLine("Distance from quickSVD to SVD: ")
 
             Console.WriteLine("Average seconds time for quick train is: " + quickTrainList.Average(x => x.Seconds));
             Console.WriteLine("Average seconds time for full train is: " + fullTrainList.Average(x => x.Seconds));
             Console.WriteLine("Average seconds time for regular SVD train is: " + regularTrainList.Average(x => x.Seconds));
-            Console.WriteLine("quickSVD is better than SVD with confidence: " + dConfidence);
+            Console.WriteLine("quickSVD is better than regular SVD with confidence: " + dConfidence);
+
+
 
         }
 
@@ -71,14 +74,63 @@ namespace Project
             throw new NotImplementedException();
         }
 
-        public SVDModel RunAlgo()
+        public SVDModel RunAlgo(int from, int to)
         {
-            throw new NotImplementedException();
+            if ((from > to) || (to > list.Count-1))
+                throw new ArgumentException();
+            if (to == from)
+            {
+                list[from].trainBaseModel(cFeatures);
+                return list[from];
+            }
+            int pivot = from + (to - from) / 2;
+
+            SVDModel a=null,b=null;
+            Task taskA = Task.Factory.StartNew(() => a = RunAlgo(from, pivot));
+            Task taskB = Task.Factory.StartNew(() => b = RunAlgo(pivot+1, pivot));
+            taskA.Wait();
+            taskB.Wait();
+            return a.merge(b);
         }
 
         public void Compute_RMSE_and_Confidence(SVDModel ourModel, SVDModel SVD, out double dConfidence, out double ourRMSE, out double svdRMSE)
         {
-            throw new NotImplementedException();
+            double predictedRankSVD, error, squared;
+            double userRMSESVD=0, moneSVD=0;
+            int AScore = 0, BScore = 0;
+            int[,] nAnB = new int[4,4]; //0- pearson, 1- cosine, 2- random, 3- svd
+            int counter = 0;
+            foreach(string user in testset.Keys)
+            {
+                foreach(string item in testset[user].Keys)
+                {
+                    counter++;
+                    #region svd_calc
+                    predictedRankSVD = SVD.PredictRating(user, item);
+                    error = testset[user][item]-predictedRankSVD;
+                    squared = Math.Pow(error, 2);
+                    userRMSESVD += squared;
+                    moneSVD += squared;
+                    #endregion
+
+                    #region our_calc
+                    OURpredictedRankSVD = ourModel.PredictRating(user, item);
+                    OURerror = testset[user][item]-predictedRankSVD;
+                    OURsquared = Math.Pow(error, 2);
+                    OURuserRMSESVD += squared;
+                    OURmoneSVD += squared;
+                    #endregion
+                }
+
+                userRMSESVD /= testset[user].Count;
+                computeRewards(userRMSESVD, OURuserRMSESVD, out AScore, out BScore);
+            }
+            //update result:
+            svdRMSE = Math.Sqrt(moneSVD / sizeOfTest);
+            //update confidence:
+
+                dConfidence =  = signTest(AScore, BScore);
+        }
         }
     }
 }
